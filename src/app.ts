960177ae02cf88f8b485cc292f78bc4905ca22b2
Zechariah <zechariahtan144@gmail.com>
2022-04-28 04:10:33 +0800

    fix: use heroku port if hosted on heroku

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,7 @@ import path from "path"
 import withErrorHandling from "./functions/withErrorHandling"
 
 const app = express()
-const PORT = 5190
+const PORT = process.env.PORT || 5190
 
 app.use(express.json())
 app.use(express.static(path.join(__dirname, "../public")))
