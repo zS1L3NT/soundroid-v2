0bea90b044db391b9ca8a03329334c4b4cb38ff5
Zechariah <zechariahtan144@gmail.com>
2022-05-01 03:22:58 +0800

    feat: add thumbnail to playlist model

--- lib/models/playlist.dart
+++ lib/models/playlist.dart
@@ -1,23 +1,27 @@
 class Playlist {
   final String name;
   final String userId;
+  final String thumbnail;
   final List<String> trackIds;
 
   Playlist({
     required this.name,
     required this.userId,
+    required this.thumbnail,
     required this.trackIds,
   });
 
   static Playlist fromJSON(Map<String, dynamic> json) => Playlist(
         name: json["name"],
         userId: json["userId"],
+        thumbnail: json["thumbnail"],
         trackIds: List<String>.from(json["trackIds"]),
       );
 
   Map<String, dynamic> toJSON() => <String, dynamic>{
         "name": name,
         "userId": userId,
+        "thumbnail": thumbnail,
         "trackIds": List<String>.from(trackIds),
       };
 }
