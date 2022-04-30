31ebc9d6a90ecb3e59665d5a38e8e759f2d02e7d
Zechariah <zechariahtan144@gmail.com>
2022-05-01 02:08:21 +0800

    feat: main navigator changes page

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:soundroid/screens/main/home_screen.dart';
+import 'package:soundroid/screens/main/library_screen.dart';
+import 'package:soundroid/screens/main/profile_screen.dart';
+import 'package:soundroid/screens/main/search_screen.dart';
 import 'package:soundroid/widgets/main/appbar.dart';
 import 'package:soundroid/widgets/main/bottom_navigation_bar.dart';
 
@@ -10,11 +14,41 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
+  int _screenIndex = 0;
+  final List<Widget> _screens = const [
+    HomeScreen(),
+    SearchScreen(),
+    LibraryScreen(),
+    ProfileScreen(),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: MainAppBar(),
-      bottomNavigationBar: const MainBottomNavigationBar(),
+      bottomNavigationBar: MainBottomNavigationBar(
+        index: _screenIndex,
+        setIndex: (index) {
+          setState(() {
+            switch (index) {
+              case 0:
+                _screenIndex = 0;
+                break;
+              case 1:
+                _screenIndex = 1;
+                break;
+              case 2:
+                _screenIndex = 2;
+                break;
+              case 3:
+                _screenIndex = 3;
+                break;
+            }
+            _screenIndex = index;
+          });
+        },
+      ),
+      body: _screens[_screenIndex],
     );
   }
 }
--- lib/widgets/main/bottom_navigation_bar.dart
+++ lib/widgets/main/bottom_navigation_bar.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
 
 class MainBottomNavigationBar extends StatefulWidget {
-  const MainBottomNavigationBar({Key? key}) : super(key: key);
+  final int index;
+  final Function(int index) setIndex;
+  const MainBottomNavigationBar({
+    Key? key,
+    required this.index,
+    required this.setIndex,
+  }) : super(key: key);
 
   @override
   State<MainBottomNavigationBar> createState() =>
@@ -9,27 +15,11 @@ class MainBottomNavigationBar extends StatefulWidget {
 }
 
 class _MainBottomNavigationBarState extends State<MainBottomNavigationBar> {
-  int _currentIndex = 0;
-
   @override
   BottomNavigationBar build(BuildContext context) {
     return BottomNavigationBar(
-      onTap: (index) {
-        setState(() {
-          switch (index) {
-            case 0:
-              break;
-            case 1:
-              break;
-            case 2:
-              break;
-            case 3:
-              break;
-          }
-          _currentIndex = index;
-        });
-      },
-      currentIndex: _currentIndex,
+      onTap: widget.setIndex,
+      currentIndex: widget.index,
       items: const [
         BottomNavigationBarItem(
           icon: Icon(Icons.home),
