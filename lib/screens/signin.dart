b48801e8219e2366abcf335e30782563ba217f8b
Zechariah <zechariahtan144@gmail.com>
2022-05-08 04:54:27 +0800

    🐛 add singlechildscrollview to prevent overflow when keyboard up

--- lib/screens/signin.dart
+++ lib/screens/signin.dart
@@ -19,158 +19,165 @@ class _SigninScreenState extends State<SigninScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: SigninAppBar(),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(
-          horizontal: 20,
-          vertical: 8,
-        ),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const Text(
-                "Welcome back",
-                style: TextStyle(
-                  fontSize: 28,
-                  fontWeight: FontWeight.w600,
-                ),
-              ),
-              const SizedBox(height: 8),
-              const Text(
-                "Sign In with your data that you entered during your registration",
-                style: TextStyle(
-                  fontSize: 15,
-                  fontWeight: FontWeight.w500,
-                ),
-              ),
-              const SizedBox(height: 32),
-              const Text(
-                "Email Address",
-                style: TextStyle(fontSize: 14),
-              ),
-              const SizedBox(height: 4),
-              TextFormField(
-                style: const TextStyle(fontSize: 15),
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  hintText: 'Enter your email address',
-                  contentPadding: EdgeInsets.symmetric(
-                    horizontal: 12,
-                    vertical: 4,
-                  ),
-                  errorStyle: TextStyle(height: 1),
-                ),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Email address cannot be empty!';
-                  }
-                  return null;
-                },
-              ),
-              const SizedBox(height: 16),
-              const Text(
-                "Password",
-                style: TextStyle(fontSize: 14),
-              ),
-              const SizedBox(height: 4),
-              TextFormField(
-                obscureText: _isObscure,
-                style: const TextStyle(fontSize: 15),
-                decoration: InputDecoration(
-                  border: const OutlineInputBorder(),
-                  hintText: 'Enter your password',
-                  contentPadding: const EdgeInsets.only(
-                    top: 4,
-                    bottom: 4,
-                    left: 12,
-                  ),
-                  errorStyle: const TextStyle(height: 1),
-                  suffixIcon: Padding(
-                    padding: const EdgeInsets.only(right: 4),
-                    child: IconButton(
-                      onPressed: () {
-                        setState(() {
-                          _isObscure = !_isObscure;
-                        });
-                      },
-                      icon: Icon(
-                        _isObscure ? Icons.visibility_off : Icons.visibility,
-                      ),
-                      splashRadius: 20,
-                    ),
-                  ),
-                ),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Password cannot be empty!';
-                  }
-                  return null;
-                },
-              ),
-              const SizedBox(height: 24),
-              SizedBox(
-                width: double.infinity,
-                height: 45,
-                child: ElevatedButton(
-                  onPressed: () {
-                    if (_formKey.currentState!.validate()) {
-                      Navigator.of(context).pushNamed(MainScreen.routeName);
-                    }
-                  },
-                  child: const Text(
-                    "Login",
+      body: SingleChildScrollView(
+        child: SizedBox(
+          height: MediaQuery.of(context).size.height -
+              MediaQuery.of(context).padding.top -
+              kToolbarHeight,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 20,
+              vertical: 8,
+            ),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    "Welcome back",
                     style: TextStyle(
-                      fontSize: 16,
+                      fontSize: 28,
+                      fontWeight: FontWeight.w600,
                     ),
                   ),
-                ),
-              ),
-              const SizedBox(height: 12),
-              Row(
-                children: const [
-                  Expanded(child: Divider()),
-                  SizedBox(width: 16),
-                  Text("or"),
-                  SizedBox(width: 16),
-                  Expanded(child: Divider()),
-                ],
-              ),
-              const SizedBox(height: 12),
-              ElevatedButton(
-                onPressed: () {},
-                child: Padding(
-                  padding: const EdgeInsets.all(12),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Image.asset(
-                        "assets/images/google-icon.png",
-                        height: 21,
+                  const SizedBox(height: 8),
+                  const Text(
+                    "Sign In with your data that you entered during your registration",
+                    style: TextStyle(
+                      fontSize: 15,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                  const SizedBox(height: 32),
+                  const Text(
+                    "Email Address",
+                    style: TextStyle(fontSize: 14),
+                  ),
+                  const SizedBox(height: 4),
+                  TextFormField(
+                    style: const TextStyle(fontSize: 15),
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: 'Enter your email address',
+                      contentPadding: EdgeInsets.symmetric(
+                        horizontal: 12,
+                        vertical: 4,
                       ),
-                      const SizedBox(width: 8),
-                      const Text(
-                        "Sign In with Google",
-                        style: TextStyle(color: Colors.black),
+                      errorStyle: TextStyle(height: 1),
+                    ),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Email address cannot be empty!';
+                      }
+                      return null;
+                    },
+                  ),
+                  const SizedBox(height: 16),
+                  const Text(
+                    "Password",
+                    style: TextStyle(fontSize: 14),
+                  ),
+                  const SizedBox(height: 4),
+                  TextFormField(
+                    obscureText: _isObscure,
+                    style: const TextStyle(fontSize: 15),
+                    decoration: InputDecoration(
+                      border: const OutlineInputBorder(),
+                      hintText: 'Enter your password',
+                      contentPadding: const EdgeInsets.only(
+                        top: 4,
+                        bottom: 4,
+                        left: 12,
                       ),
+                      errorStyle: const TextStyle(height: 1),
+                      suffixIcon: Padding(
+                        padding: const EdgeInsets.only(right: 4),
+                        child: IconButton(
+                          onPressed: () {
+                            setState(() {
+                              _isObscure = !_isObscure;
+                            });
+                          },
+                          icon: Icon(
+                            _isObscure ? Icons.visibility_off : Icons.visibility,
+                          ),
+                          splashRadius: 20,
+                        ),
+                      ),
+                    ),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Password cannot be empty!';
+                      }
+                      return null;
+                    },
+                  ),
+                  const SizedBox(height: 24),
+                  SizedBox(
+                    width: double.infinity,
+                    height: 45,
+                    child: ElevatedButton(
+                      onPressed: () {
+                        if (_formKey.currentState!.validate()) {
+                          Navigator.of(context).pushNamed(MainScreen.routeName);
+                        }
+                      },
+                      child: const Text(
+                        "Login",
+                        style: TextStyle(
+                          fontSize: 16,
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 12),
+                  Row(
+                    children: const [
+                      Expanded(child: Divider()),
+                      SizedBox(width: 16),
+                      Text("or"),
+                      SizedBox(width: 16),
+                      Expanded(child: Divider()),
                     ],
                   ),
-                ),
-                style: ElevatedButton.styleFrom(
-                  primary: Colors.white, // background
-                  onPrimary: Colors.grey[300], // foreground
-                ),
+                  const SizedBox(height: 12),
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: Padding(
+                      padding: const EdgeInsets.all(12),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Image.asset(
+                            "assets/images/google-icon.png",
+                            height: 21,
+                          ),
+                          const SizedBox(width: 8),
+                          const Text(
+                            "Sign In with Google",
+                            style: TextStyle(color: Colors.black),
+                          ),
+                        ],
+                      ),
+                    ),
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.white, // background
+                      onPrimary: Colors.grey[300], // foreground
+                    ),
+                  ),
+                  const Spacer(),
+                  Align(
+                    alignment: Alignment.center,
+                    child: TextButton(
+                      onPressed: () {},
+                      child: const Text("Forgot your password?"),
+                    ),
+                  ),
+                  const SizedBox(height: 24),
+                ],
               ),
-              const Spacer(),
-              Align(
-                alignment: Alignment.center,
-                child: TextButton(
-                  onPressed: () {},
-                  child: const Text("Forgot your password?"),
-                ),
-              ),
-              const SizedBox(height: 24),
-            ],
+            ),
           ),
         ),
       ),
