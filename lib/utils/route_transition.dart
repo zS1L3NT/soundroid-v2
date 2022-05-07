f5b88ba3724b919f173fb4b5d9e70963ce07a7d3
Zechariah <zechariahtan144@gmail.com>
2022-05-08 03:31:19 +0800

    ✨ add class for route transitions

---
+++ lib/utils/route_transition.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class RouteTransition {
+  static PageRouteBuilder fade(
+    Widget page, {
+    Duration duration = const Duration(milliseconds: 250),
+  }) {
+    return PageRouteBuilder(
+      pageBuilder: (c, anim, anim2) => page,
+      transitionsBuilder: (c, anim, anim2, child) => FadeTransition(
+        opacity: anim,
+        child: child,
+      ),
+      transitionDuration: duration,
+    );
+  }
+
+  static PageRouteBuilder slide(
+    Widget page, {
+    required Offset from,
+    Offset to = Offset.zero,
+    Duration duration = const Duration(milliseconds: 200),
+  }) {
+    return PageRouteBuilder(
+      pageBuilder: (c, anim, anim2) => page,
+      transitionsBuilder: (c, anim, anim2, child) => FadeTransition(
+        opacity: anim,
+        child: SlideTransition(
+          position: Tween<Offset>(begin: from, end: to).animate(anim),
+          child: child,
+        ),
+      ),
+      transitionDuration: duration,
+    );
+  }
+}
