cee84a00ea2d87268d53116eaead3a67c77c843a
Zechariah Tan <zechariahtan144@gmail.com>
2022-05-23 15:56:10 +0800

    🚚 move firebase to apis.ts

--- src/apis.ts
+++ src/apis.ts
@@ -1,5 +1,14 @@
+import { cert, initializeApp } from "firebase-admin/app"
 import Genius from "node-genius-api"
 import YTMusic from "ytmusic-api"
+
+export const firebaseApp = initializeApp({
+	credential: cert({
+		projectId: process.env.FIREBASE__SERVICE_ACCOUNT__PROJECT_ID,
+		clientEmail: process.env.FIREBASE__SERVICE_ACCOUNT__CLIENT_EMAIL,
+		privateKey: process.env.FIREBASE__SERVICE_ACCOUNT__PRIVATE_KEY
+	})
+})
 
 export const ytmusic = new YTMusic()
 ytmusic.initialize()
--- src/firebase.ts
+++
@@ -1,9 +0,0 @@
-import { cert, initializeApp } from "firebase-admin/app"
-
-export const firebaseApp = initializeApp({
-	credential: cert({
-		projectId: process.env.FIREBASE__SERVICE_ACCOUNT__PROJECT_ID,
-		clientEmail: process.env.FIREBASE__SERVICE_ACCOUNT__CLIENT_EMAIL,
-		privateKey: process.env.FIREBASE__SERVICE_ACCOUNT__PRIVATE_KEY
-	})
-})
