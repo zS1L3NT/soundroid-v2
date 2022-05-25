711ed08f95cbf809df4baf0f85e3c48d686af753
Zechariah <zechariahtan144@gmail.com>
2022-05-25 21:21:08 +0800

    ✨ add spotify api back to the project

--- src/@types/env.d.ts
+++ src/@types/env.d.ts
@@ -4,6 +4,10 @@ declare module NodeJS {
 		readonly FIREBASE__SERVICE_ACCOUNT__PRIVATE_KEY: string
 		readonly FIREBASE__SERVICE_ACCOUNT__CLIENT_EMAIL: string
 
+		readonly SPOTIFY__CLIENT_ID: string
+		readonly SPOTIFY__CLIENT_SECRET: string
+		readonly SPOTIFY__REFRESH_TOKEN: string
+
 		readonly GENIUS_ACCESS_TOKEN: string
 	}
 }
--- src/apis.ts
+++ src/apis.ts
@@ -1,5 +1,7 @@
 import { cert, initializeApp } from "firebase-admin/app"
+import { useTryAsync } from "no-try"
 import Genius from "node-genius-api"
+import SpotifyWebApi from "spotify-web-api-node"
 import YTMusic from "ytmusic-api"
 
 export const firebaseApp = initializeApp({
@@ -12,5 +14,36 @@ export const firebaseApp = initializeApp({
 
 export const ytmusic = new YTMusic()
 ytmusic.initialize()
+
+class Spotify extends SpotifyWebApi {
+	constructor() {
+		super({
+			clientId: process.env.SPOTIFY__CLIENT_ID,
+			clientSecret: process.env.SPOTIFY__CLIENT_SECRET,
+			refreshToken: process.env.SPOTIFY__REFRESH_TOKEN
+		})
+		this.authenticate()
+	}
+
+	async authenticate() {
+		this.setAccessToken((await this.clientCredentialsGrant()).body.access_token)
+	}
+
+	async authenticated<T>(func: () => Promise<{ body: T }>): Promise<T> {
+		const [error, data] = await useTryAsync(func)
+
+		if (error) {
+			if ((error as any).body.error.status === 401) {
+				await this.authenticate()
+				return await this.authenticated(func)
+			}
+			throw error
+		} else {
+			return data.body
+		}
+	}
+}
+
+export const spotify = new Spotify()
 
 export const genius = new Genius(process.env.GENIUS_ACCESS_TOKEN)
