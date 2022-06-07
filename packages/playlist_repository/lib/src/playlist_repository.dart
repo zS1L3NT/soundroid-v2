2d1987b29518418593f2886baf2fef32d9ac924a
Zechariah <zechariahtan144@gmail.com>
2022-06-08 03:39:13 +0800

    ✨ move playlist repository to its own package

---
+++ packages/playlist_repository/lib/src/playlist_repository.dart
@@ -0,0 +1,37 @@
+import 'dart:async';
+
+import 'package:authentication_repository/authentication_repository.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+import 'models/playlist.dart';
+
+class PlaylistRepository {
+  final _authenticationRepo = AuthenticationRepository();
+  final _collection = FirebaseFirestore.instance.collection("playlists").withConverter<Playlist>(
+        fromFirestore: (snap, _) => Playlist.fromJson(snap.data()!),
+        toFirestore: (playlist, _) => playlist.toJson(),
+      );
+
+  Stream<List<Playlist>> getPlaylists() {
+    return _collection
+        .where("userRef", isEqualTo: _authenticationRepo.currentUserRef)
+        .snapshots()
+        .map((snap) => snap.docs.map((doc) => doc.data()).toList());
+  }
+
+  Stream<Playlist?> getPlaylist(String id) {
+    return _collection.doc(id).snapshots().map((doc) => doc.data());
+  }
+
+  Future<void> addPlaylist(Playlist playlist) {
+    return _collection.add(playlist);
+  }
+
+  Future<void> updatePlaylist(Playlist playlist) {
+    return _collection.doc(playlist.id).update(playlist.toJson());
+  }
+
+  Future<void> deletePlaylist(Playlist playlist) {
+    return _collection.doc(playlist.id).delete();
+  }
+}
