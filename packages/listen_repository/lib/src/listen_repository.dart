a034bcfa8d56e89a1335ef869824bc6acf0c76fe
Zechariah Tan <zechariahtan144@gmail.com>
2022-06-08 15:25:27 +0800

    ✨ move listen repository to its own package

---
+++ packages/listen_repository/lib/src/listen_repository.dart
@@ -0,0 +1,40 @@
+import 'package:authentication_repository/authentication_repository.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+import 'models/models.dart';
+
+class ListensRepository {
+  final _authenticationRepo = AuthenticationRepository();
+  final _collection = FirebaseFirestore.instance.collection("listens").withConverter<Listen>(
+        fromFirestore: (snap, _) => Listen.fromJson(snap.data()!),
+        toFirestore: (listen, _) => listen.toJson(),
+      );
+
+  Stream<Listen?> getListen() {
+    return _collection
+        .where("userRef", isEqualTo: _authenticationRepo.currentUserRef)
+        .where("timestamp", isLessThanOrEqualTo: Timestamp.now())
+        .where(
+          "timestamp",
+          isGreaterThanOrEqualTo: Timestamp.fromDate(
+            DateTime.now().subtract(const Duration(days: 1)),
+          ),
+        )
+        .limit(1)
+        .snapshots()
+        .map((snap) => snap.docs[0].data());
+  }
+
+  Future<void> addListen(Listen listen) {
+    return _collection.add(listen);
+  }
+
+  Future<void> updateListen(Listen listen) {
+    return _collection
+        .where("userRef", isEqualTo: _authenticationRepo.currentUserRef)
+        .where("timestamp", isEqualTo: listen.timestamp)
+        .limit(1)
+        .get()
+        .then((snap) => snap.docs[0].reference.update(listen.toJson()));
+  }
+}
