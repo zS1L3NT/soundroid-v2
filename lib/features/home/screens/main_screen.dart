01a0a31b107b50ceb23079a3a592282bc392ce3f
Zechariah <zechariahtan144@gmail.com>
2022-06-18 23:04:23 +0800

    ✨ animate floating action button if no music

--- lib/features/home/screens/main_screen.dart
+++ lib/features/home/screens/main_screen.dart
@@ -1,5 +1,8 @@
+import 'package:api_repository/api_repository.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:soundroid/features/home/home.dart';
+import 'package:soundroid/features/music/music.dart';
 import 'package:soundroid/features/playlists/playlists.dart';
 import 'package:soundroid/features/search/search.dart';
 import 'package:soundroid/features/settings/settings.dart';
@@ -23,42 +26,47 @@ class _MainScreenState extends State<MainScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      extendBody: true,
-      resizeToAvoidBottomInset: false,
-      appBar: [
-        HomeAppBar(),
-        SearchAppBar(),
-        LibraryAppBar(),
-        SettingsAppBar(),
-      ][_page],
-      bottomNavigationBar: MainBottomAppBar(
-        page: _page,
-        setPage: (page) {
-          setState(() => _page = page);
-          _controller.animateToPage(
-            page,
-            duration: const Duration(milliseconds: 200),
-            curve: Curves.easeOut,
-          );
-        },
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-      floatingActionButton: const MainFloatingMusicButton(),
-      body: PageView(
-        scrollBehavior: const ScrollBehavior().copyWith(
-          overscroll: false,
-          physics: const ClampingScrollPhysics(),
-        ),
-        onPageChanged: (page) => setState(() => _page = page),
-        controller: _controller,
-        children: const [
-          HomeScreen(),
-          SearchScreen(),
-          LibraryScreen(),
-          SettingsScreen(),
-        ],
-      ),
+    return StreamBuilder<Track?>(
+      stream: context.read<MusicProvider>().current,
+      builder: (context, snap) {
+        return Scaffold(
+          extendBody: true,
+          resizeToAvoidBottomInset: false,
+          appBar: [
+            HomeAppBar(),
+            SearchAppBar(),
+            LibraryAppBar(),
+            SettingsAppBar(),
+          ][_page],
+          bottomNavigationBar: MainBottomAppBar(
+            page: _page,
+            setPage: (page) {
+              setState(() => _page = page);
+              _controller.animateToPage(
+                page,
+                duration: const Duration(milliseconds: 200),
+                curve: Curves.easeOut,
+              );
+            },
+          ),
+          floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+          floatingActionButton: snap.hasData ? const MainFloatingMusicButton() : null,
+          body: PageView(
+            scrollBehavior: const ScrollBehavior().copyWith(
+              overscroll: false,
+              physics: const ClampingScrollPhysics(),
+            ),
+            onPageChanged: (page) => setState(() => _page = page),
+            controller: _controller,
+            children: const [
+              HomeScreen(),
+              SearchScreen(),
+              LibraryScreen(),
+              SettingsScreen(),
+            ],
+          ),
+        );
+      },
     );
   }
 }
--- lib/features/home/widgets/main_bottom_app_bar.dart
+++ lib/features/home/widgets/main_bottom_app_bar.dart
@@ -1,4 +1,7 @@
+import 'package:api_repository/api_repository.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:soundroid/features/music/music.dart';
 import 'package:soundroid/widgets/widgets.dart';
 
 class MainBottomAppBar extends StatelessWidget {
@@ -19,33 +22,52 @@ class MainBottomAppBar extends StatelessWidget {
       notchMargin: 8,
       child: SizedBox(
         height: kBottomNavigationBarHeight,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-            AppIcon(
-              Icons.home_rounded,
-              color: page == 0 ? Theme.of(context).primaryColor : null,
-              onPressed: () => setPage(0),
-            ),
-            AppIcon(
-              Icons.search_rounded,
-              color: page == 1 ? Theme.of(context).primaryColor : null,
-              onPressed: () => setPage(1),
-            ),
-            const SizedBox(
-              width: 48,
-            ),
-            AppIcon(
-              Icons.library_music_rounded,
-              color: page == 2 ? Theme.of(context).primaryColor : null,
-              onPressed: () => setPage(2),
-            ),
-            AppIcon(
-              Icons.settings_rounded,
-              color: page == 3 ? Theme.of(context).primaryColor : null,
-              onPressed: () => setPage(3),
-            ),
-          ],
+        child: AnimatedContainer(
+          duration: const Duration(milliseconds: 200),
+          child: StreamBuilder<Track?>(
+            stream: context.read<MusicProvider>().current,
+            builder: (context, snap) {
+              return Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  const SizedBox(),
+                  AppIcon(
+                    Icons.home_rounded,
+                    color: page == 0 ? Theme.of(context).primaryColor : null,
+                    onPressed: () => setPage(0),
+                  ),
+                  AnimatedContainer(
+                    duration: const Duration(milliseconds: 500),
+                    width: snap.hasData ? 10 : 16,
+                  ),
+                  AppIcon(
+                    Icons.search_rounded,
+                    color: page == 1 ? Theme.of(context).primaryColor : null,
+                    onPressed: () => setPage(1),
+                  ),
+                  AnimatedContainer(
+                    duration: const Duration(milliseconds: 500),
+                    width: snap.hasData ? 88 : 16,
+                  ),
+                  AppIcon(
+                    Icons.library_music_rounded,
+                    color: page == 2 ? Theme.of(context).primaryColor : null,
+                    onPressed: () => setPage(2),
+                  ),
+                  AnimatedContainer(
+                    duration: const Duration(milliseconds: 500),
+                    width: snap.hasData ? 10 : 16,
+                  ),
+                  AppIcon(
+                    Icons.settings_rounded,
+                    color: page == 3 ? Theme.of(context).primaryColor : null,
+                    onPressed: () => setPage(3),
+                  ),
+                  const SizedBox(),
+                ],
+              );
+            },
+          ),
         ),
       ),
     );
