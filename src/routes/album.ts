559bd504372e23670d8df9676992208c9e10034f
Zechariah <zechariahtan144@gmail.com>
2022-06-20 02:03:04 +0800

    ✨ album route returns track ids

--- src/routes/album.ts
+++ src/routes/album.ts
@@ -10,17 +10,7 @@ export class GET extends Route<any, { id: string }> {
 
 	async handle() {
 		this.respond(
-			(await ytmusic.getAlbum(this.query.id)).songs
-				.map(song => ({
-					id: song.videoId,
-					title: song.name,
-					artists: song.artists.map(artist => ({
-						id: artist.artistId,
-						name: artist.name
-					})),
-					thumbnail: song.thumbnails.at(-1)?.url || ""
-				}))
-				.filter(song => song.id)
+			(await ytmusic.getAlbum(this.query.id)).songs.map(song => song.videoId).filter(Boolean)
 		)
 	}
 }
