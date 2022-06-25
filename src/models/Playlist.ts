09129d4dc993a3d13e9e3804a66d54cfcc49f431
Zechariah <zechariahtan144@gmail.com>
2022-06-26 05:32:47 +0800

    ✨ update playlist model

--- src/models/Playlist.ts
+++ src/models/Playlist.ts
@@ -1,8 +1,10 @@
-import { DocumentReference, FirestoreDataConverter } from "firebase-admin/firestore"
+import { DocumentReference, FirestoreDataConverter, Timestamp } from "firebase-admin/firestore"
 
 export default class Playlist {
 	constructor(
+		public id: String,
 		public userRef: DocumentReference,
+		public lastPlayed: Timestamp | null,
 		public name: string,
 		public thumbnail: string | null,
 		public favourite: boolean,
@@ -12,7 +14,9 @@ export default class Playlist {
 
 	static converter: FirestoreDataConverter<Playlist> = {
 		toFirestore: listen => ({
+			id: listen.id,
 			userRef: listen.userRef,
+			lastPlayed: listen.lastPlayed,
 			name: listen.name,
 			thumbnail: listen.thumbnail,
 			favourite: listen.favourite,
@@ -21,7 +25,9 @@ export default class Playlist {
 		}),
 		fromFirestore: snap =>
 			new Playlist(
+				snap.get("id"),
 				snap.get("userRef"),
+				snap.get("lastPlayed"),
 				snap.get("name"),
 				snap.get("thumbnail"),
 				snap.get("favourite"),
