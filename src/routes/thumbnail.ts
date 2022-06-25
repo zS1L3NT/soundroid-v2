e7eb96a5b92bb5659387685472dec93e2e2bf7a4
Zechariah <zechariahtan144@gmail.com>
2022-06-26 05:14:35 +0800

    ✨ allow passing an artistId for a thumbnail

--- src/routes/thumbnail.ts
+++ src/routes/thumbnail.ts
@@ -1,16 +1,21 @@
 import axios from "axios"
 import sharp from "sharp"
-import { OBJECT, STRING } from "validate-any"
+import { OBJECT, OR, STRING } from "validate-any"
 
+import { ytmusic } from "../apis"
+import processThumbnail from "../functions/processThumbnail"
 import { Route } from "../setup"
 
-export class GET extends Route<{}, { url: string }> {
-	override queryValidator = OBJECT({
-		url: STRING()
-	})
+export class GET extends Route<{}, { artistId: string } | { url: string }> {
+	override queryValidator = OR(OBJECT({ artistId: STRING() }), OBJECT({ url: STRING() }))
 
 	async handle() {
-		const imageBuffer = (await axios.get(this.query.url, { responseType: "arraybuffer" })).data
+		const url =
+			"url" in this.query
+				? this.query.url
+				: processThumbnail((await ytmusic.getArtist(this.query.artistId)).thumbnails.at(-1)?.url)
+				
+		const imageBuffer = (await axios.get(url, { responseType: "arraybuffer" })).data
 		const resizedBuffer = await sharp(imageBuffer).resize(500, 500).toBuffer()
 
 		this.res.writeHead(200, {
