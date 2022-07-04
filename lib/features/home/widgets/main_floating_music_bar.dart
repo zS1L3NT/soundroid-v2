497ed2e010eb93ea8ab5cd958545ca2de2c98ed9
Zechariah <zechariahtan144@gmail.com>
2022-07-05 02:37:15 +0800

    ✨ show error message in image cover when offline

--- lib/features/home/widgets/main_floating_music_bar.dart
+++ lib/features/home/widgets/main_floating_music_bar.dart
@@ -22,18 +22,16 @@ class MainFloatingMusicButton extends StatelessWidget {
             StreamBuilder<Track?>(
               stream: context.read<MusicProvider>().current,
               builder: (context, snap) {
-                return Padding(
-                  padding: const EdgeInsets.all(2),
-                  child: Hero(
-                    tag: "current",
-                    child: AnimatedContainer(
-                      duration: const Duration(seconds: 1),
-                      width: 60,
-                      height: 60,
-                      child: AppImage.network(
-                        snap.data?.thumbnail,
-                        borderRadius: BorderRadius.circular(30),
-                      ),
+                return Hero(
+                  tag: "current",
+                  child: AnimatedContainer(
+                    duration: const Duration(seconds: 1),
+                    width: 60,
+                    height: 60,
+                    margin: const EdgeInsets.all(2),
+                    child: AppImage.network(
+                      snap.data?.thumbnail,
+                      borderRadius: BorderRadius.circular(30),
                     ),
                   ),
                 );
--- lib/features/music/widgets/cover_image.dart
+++ lib/features/music/widgets/cover_image.dart
@@ -24,30 +24,78 @@ class CoverImage extends StatelessWidget {
             )
           : null,
       builder: (context, snap) {
-        return Hero(
-          tag: "current",
-          child: AnimatedContainer(
-            duration: const Duration(milliseconds: 500),
-            width: size,
-            height: size,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10),
-              boxShadow: [
-                if (snap.data != null && snap.data!.colors.isNotEmpty)
-                  // Render the dominant color as a shadow background for the image
-                  // to give the app a more interactive feel
-                  BoxShadow(
-                    color: snap.data!.colors.first,
-                    spreadRadius: 4,
-                    blurRadius: 20,
-                  )
+        final palette = snap.data;
+
+        return StreamBuilder<bool>(
+          stream: context.read<MusicProvider>().currentIsPlayable,
+          builder: (context, snap) {
+            final currentIsPlayable = snap.data == true;
+
+            return Stack(
+              children: [
+                Hero(
+                  tag: "current",
+                  child: AnimatedContainer(
+                    duration: const Duration(milliseconds: 500),
+                    width: size,
+                    height: size,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      boxShadow: [
+                        if (palette != null && palette.colors.isNotEmpty)
+                          // Render the dominant color as a shadow background for the image
+                          // to give the app a more interactive feel
+                          BoxShadow(
+                            color: palette.colors.first,
+                            spreadRadius: 4,
+                            blurRadius: 20,
+                          )
+                      ],
+                    ),
+                    child: AppImage.network(
+                      thumbnail,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                ),
+                FutureBuilder<bool>(
+                  future: Future.delayed(Duration.zero, () => true),
+                  builder: (context, snap) {
+                    return AnimatedOpacity(
+                      duration: const Duration(milliseconds: 500),
+                      opacity: snap.data == true ? (currentIsPlayable ? 0 : 1) : 0,
+                      child: Container(
+                        width: size,
+                        height: size,
+                        padding: const EdgeInsets.all(24),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          color: Colors.black54,
+                        ),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            AppIcon.white(Icons.cloud_off_rounded),
+                            const SizedBox(height: 8),
+                            const Material(
+                              color: Colors.transparent,
+                              child: Text(
+                                "Go online to continue listening to this track",
+                                style: TextStyle(
+                                  color: Colors.white,
+                                ),
+                                textAlign: TextAlign.center,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    );
+                  },
+                ),
               ],
-            ),
-            child: AppImage.network(
-              thumbnail,
-              borderRadius: BorderRadius.circular(10),
-            ),
-          ),
+            );
+          },
         );
       },
     );
