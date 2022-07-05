b959e424fa1bc9e3ba9b52971ef22869f2c6059d
Zechariah Tan <zechariahtan144@gmail.com>
2022-07-05 17:29:54 +0800

    ✨ add firebase auth dependency and refactor a bit

--- packages/authentication_repository/lib/src/authentication_repository.dart
+++ packages/authentication_repository/lib/src/authentication_repository.dart
@@ -1,18 +1,19 @@
-import 'package:authentication_repository/src/models/models.dart';
+import 'package:authentication_repository/authentication_repository.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart' show FirebaseAuth;
 
 /// The Authentication Repository contains all Firebase calls regarding authentication and user data.
 class AuthenticationRepository {
   /// A [DocumentReference] to the currently signed in user.
   ///
   /// A user should only be able to modify his data and no one elses.
-  final _document = FirebaseFirestore.instance
+  DocumentReference<User> get _document => FirebaseFirestore.instance
       .collection("users")
       .withConverter<User>(
         fromFirestore: (snap, _) => User.fromJson(snap.data()!),
         toFirestore: (user, _) => user.toJson(),
       )
-      .doc("jnbZI9qOLtVsehqd6ICcw584ED93");
+      .doc(FirebaseAuth.instance.currentUser!.uid);
 
   /// The document reference of the currently signed in user
   DocumentReference<User> get currentUserRef => _document;
@@ -21,12 +22,12 @@ class AuthenticationRepository {
   Stream<User?> get currentUser => _document.snapshots().map((snap) => snap.data());
 
   /// Update the currently signed in user's data
-  Future<void> updateUser(User user) {
+  void updateUser(User user) async {
     return _document.update(user.toJson());
   }
 
   /// Delete the currently signed in user's data
-  Future<void> deleteUser() {
+  void deleteUser() async {
     return _document.delete();
   }
 }
