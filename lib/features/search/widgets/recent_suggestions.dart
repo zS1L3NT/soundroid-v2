bb11836191c46ca0be589d1a35e4845ae1f0d2e8
Zechariah Tan <zechariahtan144@gmail.com>
2022-07-08 16:53:10 +0800

    ✨ default screen for no search history

--- lib/features/search/widgets/recent_suggestions.dart
+++ lib/features/search/widgets/recent_suggestions.dart
@@ -3,6 +3,7 @@ import 'package:great_list_view/great_list_view.dart';
 import 'package:provider/provider.dart';
 import 'package:search_repository/search_repository.dart';
 import 'package:soundroid/features/search/search.dart';
+import 'package:soundroid/widgets/widgets.dart';
 
 class RecentSuggestions extends StatelessWidget {
   const RecentSuggestions({Key? key}) : super(key: key);
@@ -24,6 +25,26 @@ class RecentSuggestions extends StatelessWidget {
         if (!snap.hasData) {
           return const Center(
             child: CircularProgressIndicator(),
+          );
+        }
+
+        if (snap.data!.isEmpty) {
+          return Container(
+            padding: const EdgeInsets.fromLTRB(0, 0, 0, kToolbarHeight),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                AppIcon.primaryColor(
+                  Icons.search_rounded,
+                  size: 48,
+                ),
+                const SizedBox(height: 8),
+                const Text(
+                  "Your search history will\nshow up here!",
+                  textAlign: TextAlign.center,
+                )
+              ],
+            ),
           );
         }
 
